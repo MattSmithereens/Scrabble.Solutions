0841e121550c1562d676a8d7653f59401578340c
Devin Mounts and Matt Smit <devin@>
2018-06-28 13:16:30 -0700

    estblish score variableand variables arrays for each scrabble value.

--- Scrabble/Models/ScrabbleScorer.cs
+++ Scrabble/Models/ScrabbleScorer.cs
@@ -6,8 +6,14 @@ namespace Scrabble.Models
     public class ScrabbleScorer
     {
         private string _wordString;
-        private List<int> scrabbleValue = new List<int> { } ;
-        private static Dictionary<char, int> _scrabbleValues = new Dictionary<char, int>() { { 'a', 1 }, { 'b', 3 }, { 'c', 3 }, { 'd', 2 }, { 'e', 1 }, { 'f', 4 }, { 'g', 2 }, { 'h', 4 }, { 'i', 1 }, { 'j', 8 }, { 'k', 5 }, { 'l', 1 }, { 'm', 3 }, { 'n', 1 }, { 'o', 1 }, { 'p', 3 }, { 'q', 10 }, { 'r', 1 }, { 's', 1 }, { 't', 1 }, { 'u', 1 }, { 'v', 4 }, { 'w', 4 }, { 'x', 8 }, { 'y', 4 }, { 'z', 10 } };
+        private int _score = 0;
+        private char[] _worth01 = {'a', 'e', 'i', 'o', 'u', 'l', 'n', 'r', 's', 't' };
+        private char[] _worth02 = {'d', 'g' };
+        private char[] _worth03 = {'b', 'c', 'm', 'p'};
+        private char[] _worth04 = {'f', 'h', 'v', 'w', 'y'};
+        private char[] _worth05 = {'k'};
+        private char[] _worth08 = {'j', 'x'};
+        private char[] _worth10 = {'q', 'z'};
 
 
         public ScrabbleScorer(string wordString = "")
